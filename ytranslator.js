11d75b92b37f981450be9b1d2811c13da8494e2c
okonet <andrej.okonetschnikow@gmail.com>
2010-06-20 18:09:37 +0200

    Added copyright notices

--- ytranslator.js
+++ ytranslator.js
@@ -1,3 +1,11 @@
+// 
+//  ytranslator.js
+//  YTranslator
+//  
+//  Created by Andrew Okonetchnikov on 2010-06-20.
+//  Copyright 2010 okonet.ru. All rights reserved.
+// 
+
 window.addEventListener("dblclick", getSelection);
 window.addEventListener("mouseup",  getSelection);
 
